22d2d0094fad85a2eb7410722637fb45b0348710
alfred <myfckingspamemail@gmail.com>
2023-05-03 08:39:58 +0200

    added button to make new elemnts with an maximum

--- Bookshelves/script.js
+++ Bookshelves/script.js
@@ -1,9 +1,33 @@
 
-var plus_button = 0;
+/*var plus_button = 0;
 for (let i = 0; i < 50; i ++) {
   let div = document.createElement('div');
   div.className = 'book';
   div.id = "b1_"+[i]
   const element = document.getElementById("shelv1");
   element.appendChild(div);
+}
+*/
+var max_count = 10;
+var count = 0;
+function neeew(){
+  if(count<max_count){
+    let div = document.createElement('div');
+    div.className = 'book';
+    div.id = "b1_"+[count];
+    count++;
+    let element = document.getElementById("shelv1");
+    let bttn = document.getElementById("bttn1");
+    element.insertBefore(div, bttn);
+  }
+  else{
+    let bttn = document.getElementById("bttn1");
+    bttn.remove()
+    let div = document.createElement('div');
+    div.className = 'book';
+    div.id = "b1_"+[count];
+    let element = document.getElementById("shelv1");
+    element.appendChild(div);
+    count= 0;
+  }
 }
